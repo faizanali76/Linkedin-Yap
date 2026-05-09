1970ff39173f5f3cdb54131d2381436ad4c1b7a2
Faizan <faizanalimu8@gmail.com>
2026-05-09 23:24:02 +0500

    Added Token Check (middleware)

--- src/Routes/YapRoutes.js
+++ src/Routes/YapRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
 import {handleGenerateRequest} from "../controllers/YapController.js"
+import { verifyToken } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/generate", handleGenerateRequest)
+router.post("/generate",verifyToken, handleGenerateRequest)
 
 export default router;
